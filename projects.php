4d626dae18a46fe48ec913350ebabf97aa9c6b75
sebcarss <seb.carss@gmail.com>
2015-11-16 07:32:59 +0000

    Add projects page and cs file

---
+++ projects.php
@@ -0,0 +1,11 @@
+<html>
+	<title>{ Programming Scarss }</title>
+	<body>
+		<link rel="stylesheet" href="css/projects.css" type="text/css" />
+		<?php include 'content/header.php'; ?>
+        
+        <div id="content-window">
+            
+        </div>
+    </body>
+</html>
