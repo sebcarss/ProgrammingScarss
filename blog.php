b42437df45e4af59d6353e9845d5f6bb66620624
sebcarss <seb.carss@gmail.com>
2015-11-16 07:32:01 +0000

    Add blog page and css file

---
+++ blog.php
@@ -0,0 +1,11 @@
+<html>
+	<title>{ Programming Scarss }</title>
+	<body>
+		<link rel="stylesheet" href="css/blog.css" type="text/css" />
+		<?php include 'content/header.php'; ?>
+        
+        <div id="content-window">
+            
+        </div>
+    </body>
+</html>
