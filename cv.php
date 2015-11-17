fbf90fbe72ffc9498d70565aa9e9d0dd1edd60e9
sebcarss <seb.carss@gmail.com>
2015-11-17 07:45:13 +0000

    Add charcoal grey and teal styling

--- cv.php
+++ cv.php
@@ -5,76 +5,88 @@
 		<?php include 'content/header.php'; ?>
         
         <div id="content-window">
-			<div class="leftcontent">
-				<h2>{ experience }</h2>
-			</div>
-			<div class="rightcontent">
-				<div class="experience">
-					<h3><span class="jobrole">Senior Systems Developer</span> <span class="workplace">@ Shop Direct</span></h3>
-					<ul>
-						<li>
-							I have built an in-house CMS solution using the ATG 10 Merchandising framework. This allows the content team to update the page content
-							and deploy their change within a few minutes.
-						</li>
-						<li>
-							I designed the mobile app content management system so that the content team can easily add new sections to the native app pages, as well
-							as the "web view" pages that use the apps built-in browser. 
-						</li>
-						<li>
-							I was given the role of lead developer for building the Very Exlusive site, which was delivered successfully and on time. 
-						</li>
-					</ul>
+			<div class="segment">
+				<div class="leftcontent">
+					<h2>{ experience }</h2>
+				</div>
+				<div class="rightcontent">
+					<div class="experience">
+						<h3><span class="jobrole">Senior Systems Developer</span> <span class="workplace">@ Shop Direct</span></h3>
+						<ul>
+							<li>
+								I have built an in-house CMS solution using the ATG 10 Merchandising framework. This allows the content team to update the page content
+								and deploy their change within a few minutes.
+							</li>
+							<li>
+								I designed the mobile app content management system so that the content team can easily add new sections to the native app pages, as well
+								as the "web view" pages that use the apps built-in browser. 
+							</li>
+							<li>
+								I was given the role of lead developer for building the Very Exlusive site, which was delivered successfully and on time. 
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 			
-			<div class="leftcontent">
-				<h2>{ contact details }</h2>
-			</div>
-			<div class="rightcontent">
-				<div>
-					<span>Seb Carss</span>
+			<div class="segment">
+				<div class="leftcontent">
+					<h2>{ contact details }</h2>
 				</div>
-				
-				<div>
-					<span>07877408079</span>
-				</div>
-				
-				<div>
-					<span><a href="mailto:seb.carss@gmail.com" target="_top">seb.carss@gmail.com</a></span>
-				</div>
-				
-				<div>
-					<span><a href="http://www.programmingscarss.co.uk">www.programmingscarss.co.uk</a></span>
+				<div class="rightcontent">
+					<div>
+						<span>Seb Carss</span>
+					</div>
+					
+					<div>
+						<span>07877408079</span>
+					</div>
+					
+					<div>
+						<span><a href="mailto:seb.carss@gmail.com" target="_top">seb.carss@gmail.com</a></span>
+					</div>
+					
+					<div>
+						<span><a href="http://www.programmingscarss.co.uk">www.programmingscarss.co.uk</a></span>
+					</div>
 				</div>
 			</div>
 			
-			<div class="leftcontent">
-				<h2>{ skills }</h2>
-			</div>
-			<div class="rightcontent">
-				<p>lorem ipsum</p>
-			</div>
-			
-			<div class="leftcontent">
-				<h2>{ interests }</h2>
-			</div>
-			<div class="rightcontent">
-				<p>lorem ipsum</p>
+			<div class="segment">
+				<div class="leftcontent">
+					<h2>{ skills }</h2>
+				</div>
+				<div class="rightcontent">
+					<p>lorem ipsum</p>
+				</div>
 			</div>
 			
-			<div class="leftcontent">
-				<h2>{ portfolio }</h2>
-			</div>
-			<div class="rightcontent">
-				<p>lorem ipsum</p>
+			<div class="segment">
+				<div class="leftcontent">
+					<h2>{ interests }</h2>
+				</div>
+				<div class="rightcontent">
+					<p>lorem ipsum</p>
+				</div>
 			</div>
 			
-			<div class="leftcontent">
-				<h2>{ education }</h2>
+			<div class="segment">
+				<div class="leftcontent">
+					<h2>{ portfolio }</h2>
+				</div>
+				<div class="rightcontent">
+					<p>lorem ipsum</p>
+				</div>
 			</div>
-			<div class="rightcontent">
-				<h3>Liverpool John Moores University</h3>
-				<div>Bachelor of Science (Hons) in <span class="degree">Computer Games Technology</span></div>
+			
+			<div class="segment">
+				<div class="leftcontent">
+					<h2>{ education }</h2>
+				</div>
+				<div class="rightcontent">
+					<h3>Liverpool John Moores University</h3>
+					<div>Bachelor of Science (Hons) in <span class="degree">Computer Games Technology</span></div>
+				</div>
 			</div>
             <!--
                 experience
