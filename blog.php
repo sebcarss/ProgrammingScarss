fa3f9a9911a025c013f0419cc8a259a372c4078b
sebcarss <seb.carss@gmail.com>
2015-12-10 20:25:01 +0000

    Add composer and dependency on monolog

--- blog.php
+++ blog.php
@@ -4,13 +4,18 @@
         <title>Programming Scarss</title>
     </head>
     <body>
-        <div class="post">
+        <div class="header">
+            Blog Posts
+        </div>
+        <div class="posts">
             <?php
                 $posts = $db->getLatestPosts();
                 
                 foreach ($posts as $doc) {
+                    echo '<div class="post">';
                     echo '<a href="blog/' . $doc['permalink'] . '.php">' . $doc['title'] . '</a>';
                     echo '<p>' . $doc['blurb'] . '</p>';
+                    echo '</div>';
                 }
             ?>
         </div>
