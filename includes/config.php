e22781b7e86cc77ff15c68b5433a8fd3839fa4b6
sebcarss <seb.carss@gmail.com>
2015-12-17 21:55:38 +0000

    Add Flight routing

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,19 @@
 <?php
+    include_once 'vendor/autoload.php';
     require_once '/classes/DB.class.php';
     //require_once '/classes/User.class.php';
     
     session_start();
+    
+    Flight::route('GET /', function() {
+        echo 'You are on the home page';
+    });
+    
+    Flight::route('GET /blog.php', function() {
+        echo 'You are on the blog home page';
+    });
+    
+    Flight::start();
     
     // Connect to database
     //$db = new DB('programmingscarss');
